96e43432154346b476ce930d1d6f47fda9af16e1
Egor <aa_dimidrol@mail.ru>
2022-10-23 21:04:52 +0300

    Update time sheduler

--- otchet/updater.py
+++ otchet/updater.py
@@ -2,5 +2,5 @@ from apscheduler.schedulers.background import BackgroundScheduler
 from .something_update import update_something, print_hola
 
 scheduler = BackgroundScheduler()
-scheduler.add_job(update_something, 'interval', seconds=10)
-scheduler.add_job(print_hola, 'interval', seconds=5)
+scheduler.add_job(update_something, 'interval', seconds=100)
+scheduler.add_job(print_hola, 'interval', seconds=50)
